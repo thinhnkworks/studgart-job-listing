7f3d6768c4c8b79a24f455eb0f731080530a3a61
thanhloi12345 <tranthanhloi13082003@gmail.com>
2024-09-21 13:34:19 +0700

    feat (backend): credential google authen

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes";
+import connectDB from "./config/mongoose";
+import cors from "cors";
+import setupSwagger from "./swagger";
 
 // Load environment variables
 dotenv.config();
+
 // Connect to MongoDB
+
 const app = express();
+
 // Middleware
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server running on port ${process.env.PORT || 3000}`);
+setupSwagger(app);
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+connectDB().then((res) => {
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server running on port ${process.env.PORT || 3000}`);
+  });
 });
